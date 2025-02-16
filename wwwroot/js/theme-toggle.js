01e945f0cb7ed3b39ff1adf4971d953f7d2fd226
Ryan Maxie <MichaelRMaxie@Gmail.com>
2025-02-16 00:38:32 -0600

    Refactor layout Enhance theme toggle functionality with improved styles and structure

--- wwwroot/js/theme-toggle.js
+++ wwwroot/js/theme-toggle.js
@@ -3,10 +3,15 @@ function toggleTheme() {
   // Get the theme link element
   const themeLink = document.getElementById('theme-link');
 
-  // If the current theme is light, change it to dark
-  if (themeLink.getAttribute('href') === 'css/app.css') {
-    themeLink.setAttribute('href', 'css/dark-theme.css');
-  } else { // Otherwise, change it back to light
+  // If the current theme is dark, change it to light
+  if(themeLink.getAttribute('href') === 'css/dark-theme.css') {
     themeLink.setAttribute('href', 'css/app.css');
+  } else { // Otherwise, change it to dark
+    themeLink.setAttribute('href', 'css/dark-theme.css');
   }
+}
+
+function blurActiveElement() {
+    // Blur the active element (the button)
+    document.activeElement.blur();
 }
