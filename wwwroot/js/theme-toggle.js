36189ec4aa94f7b613eb0445a40d0bb8d36f830c
Ryan Maxie <MichaelRMaxie@Gmail.com>
2025-02-16 02:39:29 -0600

    Add icons to theme toggle button and update styles

--- wwwroot/js/theme-toggle.js
+++ wwwroot/js/theme-toggle.js
@@ -3,11 +3,23 @@ function toggleTheme() {
   // Get the theme link element
   const themeLink = document.getElementById('theme-link');
 
+  // Get the two icons for the toggle button
+  const sunIcon = document.getElementById('toggle-sun-icon');
+  const moonIcon = document.getElementById('toggle-moon-icon');
+
   // If the current theme is dark, change it to light
   if(themeLink.getAttribute('href') === 'css/dark-theme.css') {
     themeLink.setAttribute('href', 'css/app.css');
+
+    // Change the icons
+    sunIcon.style.display = 'none';
+    moonIcon.style.display = 'block';
   } else { // Otherwise, change it to dark
     themeLink.setAttribute('href', 'css/dark-theme.css');
+
+    // Change the icons
+    sunIcon.style.display = 'block';
+    moonIcon.style.display = 'none';
   }
 }
 
